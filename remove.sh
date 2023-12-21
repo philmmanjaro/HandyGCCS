c8983538cfad734e53f7d81a7a74c16d232ef693
Philip Mueller <philm@manjaro.org>
2023-12-21 15:14:58 +0700

    [remove] make sure all udev hwdb get removed

--- remove.sh
+++ remove.sh
@@ -5,7 +5,7 @@ sudo rm /usr/bin/handycon
 sudo rm -rf /usr/share/handygccs
 sudo rm -rf /etc/handygccs
 sudo rm /usr/lib/systemd/system/handycon.service
-sudo rm /usr/lib/udev/hwdb.d/59-handygccs-ayaneo.hwdb
+sudo rm /usr/lib/udev/hwdb.d/59-handygccs-*.hwdb
 sudo rm /usr/lib/udev/rules.d/60-handycon.rules
 sudo systemd-hwdb update
 sudo udevadm control -R
